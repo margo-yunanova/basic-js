4cf2d777eb1f48bfabed2fa5c12e6923821702ba
Margo <m.yunanova@gmail.com>
2023-12-14 13:32:16 +0300

    feat: solution for encoder line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function encodeLine(str) {
+    let encodeStr = "";
+    let counter = 1;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[i + 1]) {
+            encodeStr += (counter === 1 ? "" : counter) + str[i];
+            counter = 1;
+            continue;
+        }
+        counter++;
+    }
+    return encodeStr;
 }
 
+// function encodeLine(str) {
+//     const newArr = [];
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === str[i - 1]) {
+//             let index = newArr.findIndex(([n, l]) => l === str[i]);
+//             index = index === -1 ? newArr.length : index;
+//             newArr[index][0] = newArr[index][0] + 1;
+//         } else {
+//             newArr.push([1, str[i]]);
+//         }
+//     }
+//     return newArr
+//         .map(([n, l]) => (n === 1 ? l : [n, l]))
+//         .flat()
+//         .join("");
+// }
+
 module.exports = {
-  encodeLine
+    encodeLine,
 };
